73591cb14c1a00d273ef6aa975ddecb7761437ef
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-04-22 13:06:01 +0200

    Add basic n-gram implementation for strings

---
+++ src/DistanceEverything.Tests/NgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using static AssertNet.Assertions;
+
+namespace DistanceEverything.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="Ngram"/> class.
+    /// </summary>
+    public static class NgramTests
+    {
+        /// <summary>
+        /// Checks that string inputs respond with the correct distance.
+        /// </summary>
+        /// <param name="a">The first string.</param>
+        /// <param name="b">The second string.</param>
+        /// <param name="expectedValue">The expected value.</param>
+        [Theory]
+        [InlineData("abcd", "abcd", 0)]
+        [InlineData("abcde", "abcd", 1)]
+        [InlineData("abcdef", "abcd", 2)]
+        [InlineData("abcd", "abcdef", 2)]
+        [InlineData("zzabcd", "abcdef", 4)]
+        public static void Strings(string a, string b, int expectedValue)
+            => AssertThat(Ngram.Distance(2, a, b)).IsEqualTo(expectedValue);
+    }
+}
---
+++ src/DistanceEverything/Ngram.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DistanceEverything
+{
+    /// <summary>
+    /// Used for computing n-gram distances.
+    /// </summary>
+    public static class Ngram
+    {
+        /// <summary>
+        /// Computes the n-gram distance between two strings.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <param name="s">The first string.</param>
+        /// <param name="t">The second string.</param>
+        /// <returns>The n-gram distance between the given strings.</returns>
+        public static int Distance(int n, string s, string t)
+        {
+            HashSet<string> sl = new HashSet<string>();
+            HashSet<string> tl = new HashSet<string>();
+
+            for (int i = 0; i < s.Length - n + 1; i++)
+            {
+                sl.Add(s.Substring(i, n));
+            }
+
+            for (int i = 0; i < t.Length - n + 1; i++)
+            {
+                tl.Add(t.Substring(i, n));
+            }
+
+            int intersection = sl.Intersect(tl).Count();
+
+            return sl.Count + tl.Count - (2 * intersection);
+        }
+    }
+}
